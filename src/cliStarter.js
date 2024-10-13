d13e4b1657d578991f0c1a40a63abb03d0b793c0
Aleksei Golubkov <nuriver@gmail.com>
2024-10-13 12:30:28 +0200

    feat: implement up command functionality

--- src/cliStarter.js
+++ src/cliStarter.js
@@ -6,6 +6,7 @@ import readline from "node:readline/promises";
 const cliStarter = () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
+  let currentDir = __dirname;
   const rl = readline.createInterface(input, output);
   let username = process.env.npm_config_username;
 
@@ -14,16 +15,32 @@ const cliStarter = () => {
   }
 
   rl.write(`Welcome to the File Manager, ${username}!\n`);
-  rl.write(`You are currently in ${__dirname}\n`);
+  rl.write(`You are currently in ${currentDir}\n`);
   rl.setPrompt("Enter a command: ");
   rl.prompt();
   rl.on("line", (input) => {
     const trimmedInput = input.toString().trim();
+
     if (trimmedInput === ".exit") {
       rl.close();
+      return;
     }
-  });
 
+    switch (trimmedInput) {
+      case "up":
+        currentDir = path.resolve(currentDir, "..");
+        break;
+      case "test":
+        console.log("Test command was entered");
+        break;
+      default:
+        console.log("Invalid input");
+        break;
+    }
+
+    console.log(`You are currently in ${currentDir}`);
+    rl.prompt();
+  });
   rl.on("close", () => {
     output.write(`\nThank you for using File Manager, ${username}, goodbye!\n`);
   });
